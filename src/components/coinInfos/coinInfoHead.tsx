bbdda0cbeda362ec29e0d852101a0416db3faea7
Roy Choi <judoaseeta@gmail.com>
2021-03-27 01:20:02 +0900

    마켓 캡 => 시가총액

--- src/components/coinInfos/coinInfoHead.tsx
+++ src/components/coinInfos/coinInfoHead.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './styles/coinInfoHead.module.scss';
 
-const headList = ['화폐명', '거래가', '일일 거래량','마켓 캡', '일주일 변동차트', '24시간 변동율'];
+const headList = ['화폐명', '거래가', '일일 거래량','시가 총액', '일주일 변동차트', '24시간 변동율'];
 const CoinInfoHead: React.FC = () => 
 <thead
     className={styles.head}
